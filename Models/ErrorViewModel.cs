0efe2e1b1842fe95d8954091c97643a518cec46f
Greg Bowne <gbowne144@gmal.com>
2021-08-10 22:25:59 -0700

    Initial Commit A

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace spmssite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
